9cd7c197345eacc7aac28f9fefdb6c09aae36bb4
Paul Schuetz <pa.schuetz@web.de>
2026-04-22 21:48:57 +0200

    Implement main app screen with task list
    creates the main app screen with the task list and all its
    indications of number & evaluations today and over a time period.

--- Confident Attempt/Confident_AttemptApp.swift
+++ Confident Attempt/Confident_AttemptApp.swift
@@ -8,6 +8,6 @@ struct Confident_AttemptApp: App {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: Task.self)
+        .modelContainer(for: Task.self, inMemory: true)
     }
 }
--- Confident Attempt/ContentView.swift
+++ Confident Attempt/ContentView.swift
@@ -4,12 +4,58 @@ import Confident_Attempt_Model
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Task]
+    @Query(sort: \Task.name) private var items: [Task]
+    
+    private var relevantPeriod = CalculationStart.months(number: 1)
+    private var expectedOverall = 0.75
 
+    private var today: DateComponents {
+        Date.now.dc
+    }
+    
+    private var floatStyle: FloatingPointFormatStyle<Double> {
+        .number.precision(.fractionLength(0...2))
+    }
+    
+    private var percentStyle: FloatingPointFormatStyle<Double>.Percent {
+        FloatingPointFormatStyle<Double>.Percent().precision(.fractionLength(0))
+    }
+    
     var body: some View {
         List(items) { item in
             HStack {
-                Text(item.name)
+                getTaskSymbol(item)
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(item.name)
+                        
+                        Spacer()
+                        
+                        Text(getTaskExpected(item))
+                    }
+                    HStack {
+                        Text("Completion: \(item.getEvaluation(from: relevantPeriod, to: today).formatted(percentStyle))")
+                        
+                        Spacer()
+                        
+                        Text(getTaskText(item))
+                    }
+                }
+            }
+            .foregroundStyle(getForegroundColour(item))
+            .swipeActions(edge: .trailing) {
+                Button("Decrease", systemImage: "minus.circle") {
+                    withAnimation {
+                        item.decreaseDay(today, by: 1)
+                    }
+                }
+            }
+            .swipeActions(edge: .leading) {
+                Button("Increase", systemImage: "plus.circle") {
+                    withAnimation {
+                        item.increaseDay(today, by: 1)
+                    }
+                }
             }
         }
         .toolbar {
@@ -18,6 +64,76 @@ struct ContentView: View {
                     
                 }
             }
+        }
+    }
+    
+    private func getTaskSymbol(_ task: Task) -> some View {
+        let number = task.getDay(today)
+        
+        var result = Image(systemName: "number.circle.fill")
+        
+        if number == 0 {
+            result = Image(systemName: "xmark.circle.fill")
+        } else if let max = task.maxNum, max == 1 && number == 1 {
+            result = Image(systemName: "checkmark.circle.fill")
+        } else if number <= 50 {
+            result = Image(systemName: "\(number).circle.fill")
+        }
+        
+        if task.getEvaluationForDay(today) >= 1.0 {
+            return result.foregroundStyle(.green)
+        } else {
+            return result.foregroundStyle(.red)
+        }
+    }
+    
+    private func getTaskText(_ task: Task) -> String {
+        let number = task.getDay(today)
+        
+        var result = "Today: \(number)"
+        
+        if let max = task.maxNum {
+            if max == 1 {
+                if number == 1 {
+                    result = "Done"
+                } else {
+                    result = "Yet to do"
+                }
+                
+                result += " today"
+            } else {
+                result += " / \(max)"
+            }
+        }
+        
+        return result
+    }
+    
+    private func getTaskExpected(_ task: Task) -> String {
+        let expected = task.expectedNum
+        let period = switch expected {
+            case .daily(let expectedNum):
+                "\(expectedNum) daily"
+            case .weekly(let expectedNum):
+                "\(expectedNum) weekly"
+            case .monthly(let expectedNum):
+                "\(expectedNum) monthly"
+            case .yearly(let expectedNum):
+                "\(expectedNum) yearly"
+        }
+        
+        return "Expected: \(period)"
+    }
+    
+    private func getForegroundColour(_ task: Task) -> Color {
+        let eval = task.getEvaluation(from: relevantPeriod, to: today)
+        
+        return if eval < expectedOverall {
+            .red
+        } else if eval >= 1.0 {
+            .green
+        } else {
+            .primary
         }
     }
 }
@@ -32,9 +148,9 @@ func getContainer() -> ModelContainer {
     let container = try! ModelContainer(for: Task.self, configurations: config)
     
     let task1 = Task(name: "Test 1", textDescription: "Test")
-    let task2 = Task(name: "Test 2", textDescription: "Test")
-    let task3 = Task(name: "Test 3", textDescription: "Test")
-    let task4 = Task(name: "Test 4", textDescription: "Test")
+    let task2 = Task(name: "Test 2", textDescription: "Test", maxNum: .none)
+    let task3 = Task(name: "Test 3", textDescription: "Test", maxNum: 10)
+    let task4 = Task(name: "Test 4", textDescription: "Test", expectedNum: .weekly(number: 3))
     container.mainContext.insert(task1)
     container.mainContext.insert(task2)
     container.mainContext.insert(task3)
