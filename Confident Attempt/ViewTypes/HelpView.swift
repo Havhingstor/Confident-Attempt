31d948b8fa98d7c477d50306355ecbafaab43994
Paul Schuetz <pa.schuetz@web.de>
2026-05-04 23:08:11 +0200

    Set version to 0.1.0, improve help section
    The help section now shows the correct medium colour and version
    string, which is set to 0.1.0

--- Confident Attempt/ViewTypes/HelpView.swift
+++ Confident Attempt/ViewTypes/HelpView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct HelpView: View {
+    let appVersion = (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String) ?? ""
+
     var body: some View {
         List {
             Section {
@@ -11,13 +13,14 @@ struct HelpView: View {
                 Link(destination: URL(string: "https://github.com/Havhingstor/Confident-Attempt")!, label: {
                     Text("GitHub")
                 })
+                Text("Version \(appVersion)")
             }
 
             Section("Tips") {
                 Text("Swipe right or left to increase or decrease the number of times you have completed a habit.")
                 Text("A habit can be completed either once per day ('normal'), multiple times up to a certain limit ('repeated'), or as often as you like ('unlimited').")
                 Text("The symbol on the left is green if today's completions are enough to reach the long-term goal if repeated daily, and red if this is not the case.")
-                Text("The remaining text is red if the number of completions is below the minimum for the given period (both of which can be configured in the settings), and green if the number of completions is above the goal. Otherwise, it is black.")
+                Text("The remaining text is red if the number of completions is below the minimum for the given period (both of which can be configured in the settings), and green if the number of completions is above the goal. Otherwise, it is yellow.")
             }
         }
         .navigationTitle("Help")
